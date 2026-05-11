ef1000277496fd1da261bdfdb21f6160d2823d6b
wakinoza <wakinoza@gmail.com>
2026-05-11 16:54:47 +0900

    Spring Boot環境を構築

---
+++ todo_servlet_db_app/src/main/java/com/example/demo/SpringtTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtTodoAppApplication.class, args);
+	}
+
+}
