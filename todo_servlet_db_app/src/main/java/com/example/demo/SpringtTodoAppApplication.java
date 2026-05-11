7c541cbed299e988e26147eab5de1c2bd9c1bb7c
wakinoza <wakinoza@gmail.com>
2026-05-11 17:05:08 +0900

    DB設定が完了するまで、一時的にExcludeを追加して、errorを解除する

--- todo_servlet_db_app/src/main/java/com/example/demo/SpringtTodoAppApplication.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/SpringtTodoAppApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringtTodoAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringtTodoAppApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringtTodoAppApplication.class, args);
+  }
 
 }
