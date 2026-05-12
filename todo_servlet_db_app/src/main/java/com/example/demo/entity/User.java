2d6bdcb20fba00c0c6833be6a4fe1150e5e6d141
wakinoza <wakinoza@gmail.com>
2026-05-12 15:32:40 +0900

    beaｎパッケージのＵｓｅｒ．ｊａｖａをＥｎｔｉｔｙパッケージに移行し・Ｓｐｒｉｎｇ構成に変更した

---
+++ todo_servlet_db_app/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * . ユーザー情報を保持するクラス
+ */
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+  /** . ユーザーID */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  /** . ユーザー名 */
+  @Column(nullable = false, length = 50)
+  private String name;
+
+  /** . パスワード */
+  @Column(nullable = false, unique = true, length = 100)
+  private String password;
+}
