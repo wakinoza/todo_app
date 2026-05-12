94c1c4ceb0bf7930f5a50a07268831c7385d101b
wakinoza <wakinoza@gmail.com>
2026-05-12 16:04:23 +0900

    javadocの追加

--- todo_servlet_db_app/src/main/java/com/example/demo/repository/TodoItemRepository.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/repository/TodoItemRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.entity.TodoItem;
 
+/**
+ * . ToDoItemのリポジトリー
+ */
 @Repository
 public interface TodoItemRepository extends JpaRepository<TodoItem, Integer> {
 
