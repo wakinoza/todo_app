0e9899b1dab6fdd3c64b26a6210439048780b016
wakinoza <wakinoza@gmail.com>
2026-05-12 16:25:08 +0900

    UserServiceの作成

---
+++ todo_servlet_db_app/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * . Userクラスのサービスクラス
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository repository;
+  private final PasswordEncoder passwordEncoder;
+
+  /**
+   * . ログイン認証メソッド
+   * 
+   * @param name ユーザー名
+   * @param rawPassword パスワード
+   * @return 認証済みユーザー
+   */
+  public User login(String name, String rawPassword) {
+    return repository.findByName(name)
+        .filter(user -> passwordEncoder.matches(rawPassword, user.getPassword())) // 照合
+        .orElse(null);
+  }
+}
