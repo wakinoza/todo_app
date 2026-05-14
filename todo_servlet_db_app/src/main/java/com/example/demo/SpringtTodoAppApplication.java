92cda78793470e8e8c358f822ccad7df518245ca
wakinoza <wakinoza@gmail.com>
2026-05-14 16:44:56 +0900

    アプリの動作確認のため、一時的にSecutiryをOFFにする。セキュリティ実装時に解除する予定

--- todo_servlet_db_app/src/main/java/com/example/demo/SpringtTodoAppApplication.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/SpringtTodoAppApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(
+    exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class SpringtTodoAppApplication {
 
   public static void main(String[] args) {
