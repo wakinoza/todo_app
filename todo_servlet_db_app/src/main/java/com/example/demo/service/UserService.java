82e8c0867e76a2244b4782e74792204432de29ae
wakinoza <wakinoza@gmail.com>
2026-05-14 17:53:35 +0900

    デバック用コードを追加

--- todo_servlet_db_app/src/main/java/com/example/demo/service/UserService.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/service/UserService.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * . Userクラスのサービスクラス
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -16,15 +18,25 @@ public class UserService {
   private final UserRepository repository;
   private final PasswordEncoder passwordEncoder;
 
-  /**
-   * . ログイン認証メソッド
-   * 
-   * @param name ユーザー名
-   * @param rawPassword パスワード
-   * @return 認証済みユーザー
-   */
+  // デバック用
   public User login(String name, String rawPassword) {
-    return repository.findByName(name)
-        .filter(user -> passwordEncoder.matches(rawPassword, user.getPassword())).orElse(null);
+    log.error("★ログイン試行: name={}", name);
+    return repository.findByName(name).filter(user -> {
+      boolean match = passwordEncoder.matches(rawPassword, user.getPassword());
+      log.error("★パスワード照合結果: {}", match);
+      return match;
+    }).orElse(null);
   }
+  // 本番用
+  // /**
+  // * . ログイン認証メソッド
+  // *
+  // * @param name ユーザー名
+  // * @param rawPassword パスワード
+  // * @return 認証済みユーザー
+  // */
+  // public User login(String name, String rawPassword) {
+  // return repository.findByName(name)
+  // .filter(user -> passwordEncoder.matches(rawPassword, user.getPassword())).orElse(null);
+  // }
 }
