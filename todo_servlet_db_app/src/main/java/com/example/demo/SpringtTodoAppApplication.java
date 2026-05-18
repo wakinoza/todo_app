89178aec44166ffa25719813e22cbb6d22aa6e4a
wakinoza <wakinoza@gmail.com>
2026-05-18 16:54:10 +0900

    アプリのログイン挙動を確認するために、一時的にSpringSecurity機能を解除

--- todo_servlet_db_app/src/main/java/com/example/demo/SpringtTodoAppApplication.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/SpringtTodoAppApplication.java
@@ -2,11 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class SpringtTodoAppApplication {
 
   public static void main(String[] args) {
---
+++ todo_servlet_db_app/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(CsrfConfigurer::disable).authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        .formLogin(FormLoginConfigurer::disable);
+
+    return http.build();
+  }
+}
