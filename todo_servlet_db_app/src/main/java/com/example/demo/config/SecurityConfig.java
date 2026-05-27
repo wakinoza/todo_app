bc2889f6e886231a28c85173ce6015317571f00d
wakinoza <wakinoza@gmail.com>
2026-05-27 17:42:31 +0900

    ログアウトログを記録するため、logoutSuccessUrlをlogoutSuccessHandlerに変更

--- todo_servlet_db_app/src/main/java/com/example/demo/config/SecurityConfig.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import com.example.demo.entity.User;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
 
   @Bean
@@ -19,8 +22,20 @@ public class SecurityConfig {
         .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/todo/list", true)
             .loginProcessingUrl("/login").permitAll())
 
-        .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?logout")
-            .invalidateHttpSession(true).deleteCookies("JSESSIONID"));
+        .logout(logout -> logout.logoutUrl("/logout").invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler((request, response, authentication) -> {
+
+              if (authentication != null && authentication.getPrincipal() instanceof User) {
+                User loginUser = (User) authentication.getPrincipal();
+                log.info("ユーザーがログアウトしました。ユーザー名: [{}] (ID: {})", loginUser.getName(),
+                    loginUser.getId());
+              } else {
+                log.info("セッションが存在しない、またはゲストがログアウトしました。");
+              }
+
+              response.sendRedirect("/login?logout");
+            }));
 
     return http.build();
   }
