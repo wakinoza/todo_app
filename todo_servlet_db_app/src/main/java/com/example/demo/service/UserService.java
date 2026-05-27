c5d9d72535f583a7faaf8d364c029037a9721614
wakinoza <wakinoza@gmail.com>
2026-05-27 17:33:43 +0900

    loginメソッドの削除（ＳｐｒｉｎｇＳｅｃｕｒｉｔｙ導入により、メソッドが不要になった）
    今後の機能追加のため、クラスは空のまま維持しておく。

--- todo_servlet_db_app/src/main/java/com/example/demo/service/UserService.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/service/UserService.java
@@ -1,42 +1,14 @@
 package com.example.demo.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * . Userクラスのサービスクラス
+ * Userクラスのサービスクラス (現在、新規ユーザー登録機能はないため、DIの器として定義)
  */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
-  private final UserRepository repository;
-  private final PasswordEncoder passwordEncoder;
-
-  // デバック用
-  public User login(String name, String rawPassword) {
-    log.error("★ログイン試行: name={}", name);
-    return repository.findByName(name).filter(user -> {
-      boolean match = passwordEncoder.matches(rawPassword, user.getPassword());
-      log.error("★パスワード照合結果: {}", match);
-      return match;
-    }).orElse(null);
-  }
-  // 本番用
-  // /**
-  // * . ログイン認証メソッド
-  // *
-  // * @param name ユーザー名
-  // * @param rawPassword パスワード
-  // * @return 認証済みユーザー
-  // */
-  // public User login(String name, String rawPassword) {
-  // return repository.findByName(name)
-  // .filter(user -> passwordEncoder.matches(rawPassword, user.getPassword())).orElse(null);
-  // }
 }
