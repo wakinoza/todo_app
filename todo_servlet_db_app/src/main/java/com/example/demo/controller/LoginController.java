2f707100fa4a107388ba433a62a3f73ce778f753
wakinoza <wakinoza@gmail.com>
2026-05-27 17:12:01 +0900

    ログコードの追加

--- todo_servlet_db_app/src/main/java/com/example/demo/controller/LoginController.java
+++ todo_servlet_db_app/src/main/java/com/example/demo/controller/LoginController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
 
 
 @Controller
+@Slf4j
 public class LoginController {
 
   /**
@@ -12,6 +14,7 @@ public class LoginController {
    */
   @GetMapping("/login")
   public String viewLogin() {
+    log.info("ログイン画面（/login）がリクエストされました。");
     return "login";
   }
 
